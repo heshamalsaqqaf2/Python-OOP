ad4a3437595eadbf47e64a13bccf1b4e4a04c984
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-08-10 20:42:45 +0300

    Object oriented programming concepts in Python.

---
+++ 01-Class-And-Object.py
@@ -0,0 +1,39 @@
+"""
+ ? CLASS AND OBJECT:
+   1- The concept of classics
+   2- Class definition
+   3- Object concept
+   4- The concept of properties
+   5- The word self
+   6- Function __init__()
+   7- Relationship between class and object
+"""
+
+# Class Concept:
+# TODO: Class This is a container that can contain functions, variables, arrays, etc.
+
+# Object Concept:
+# TODO: Object is a copy of the class.
+
+# Class Definition
+
+# Concept Of Properties
+
+# The Word Self
+
+# Function __init__()
+
+# Relationship Between Class And Object
+"""
+  TODO: Class:
+  The basic idea of a class is to set up a common format for storing data and provide user-friendly
+    ways to access and manipulate that data seamlessly. By its very nature, 
+    Glass doesn't store any information,
+    which is why it's called a Blue Print.
+
+  TODO: Object:
+  The basic idea of an object is to create a replica of the Glass and insert the data
+    you want into it while respecting any conditions set in the original Glass. 
+    So you can't create an object without a class because
+    an object by its very nature is a copy of a specific class.
+"""
---
+++ 02-Inheritance.py
@@ -0,0 +1,36 @@
+"""
+ ? INHERITANCE:
+   1- The concept of inheritance
+   2- How to make a class inherit from another class
+   3- Forms of inheritance in Python
+   4- Knowing the nature of the relationship between Classes and Objects
+   5- Name conflicts when inheriting more than one class
+   6- Resolving name conflicts
+   7- Class objects
+"""
+
+# Concept Of Inheritance
+"""
+ TODO: Inheritance:
+  means embedding the content of a class into another class.
+  In Python, a class can inherit from another class and thus get all the functions and variables in it.
+  Inheritance can be used to reduce code duplication,
+  for example, if you want to create a new class and you have a ready-made
+  class with variables and functions that you need to write in it,
+  you can make the class inherit them as they are instead of redefining them in it,
+  and then you can use all the variables and functions that the new class inherited
+  from the ready-made class.
+"""
+
+
+# How to make a class inherit from another class
+
+# Forms of inheritance in Python
+
+# Knowing the nature of the relationship between Classes and Objects
+
+# Name conflicts when inheriting more than one class
+
+# Resolving name conflicts
+
+# Class objects
